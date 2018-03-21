22087d45126610e4e77e03486cc6536e78035066
amjayy <33906054+amjayy@users.noreply.github.com>
2018-03-21 15:45:19 -0500

    Add files via upload

---
+++ new js/javascript/string-variable.js
@@ -0,0 +1,10 @@
+var username;
+var message;
+username = 'Amber';
+message = 'Press this button';
+
+var elName= document.elElementById('name');
+elName.textContent= username;
+var elNote = document.getElementById('note);
+elNote.textContent = message;
+
---
+++ new js/javascript/string-with-quotes.js
@@ -0,0 +1,9 @@
+var title;
+var message;
+title = "AHAHA You THOUGHT That Last Button Worked";
+message ='<a href=\"sale.html\">Here is 25%off,Dummy!</a';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = message;
